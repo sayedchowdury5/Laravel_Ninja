44363108d3ed79626b14d17f9eb0a8056de8fda2
Abu Sayed <sayedchowdury5@gmail.com>
2020-08-16 00:03:44 +0800

    Launched the website

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PizzaController extends Controller
+{
+    public function index(){
+        $pizzas = [
+            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+            ['type' => 'volcano', 'base' => 'garlic crust'],
+            ['type' => 'veg supreme', 'base' => 'thin & crispy']
+        ];
+    
+        // Querry parametes using request method 
+        return view('Pizzas', [
+            'pizzas' => $pizzas,
+            'name' => request('name'),
+            'age' => request('age')
+            ]);
+    
+    }
+
+    public function show($id){
+        return view('details', ['id' => $id ]);
+    }
+}
